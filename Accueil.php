64d0f6833a673e1de0b01d5ec4429f4ee0a94b84
Your Name <you@example.com>
2022-03-07 17:06:00 +0100

    maj

---
+++ Accueil.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="shortcut icon" href="./img1/ang4.jpg>
+    
+    <title>Accueil</title>
+<head>
+</head>
+
+<body>
+    <?php
+        require_once('barreMenu.php')
+        
+    ?>
+    
+</body>
+</html>
---
+++ barreMenu.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/style.css"/>
+  
+  <title>Bienvenue à tous</title>
+
+</head>
+<header>
+</header>
+<body>
+    <nav class="cc-navbar navbar navbar-expand-lg position-fixed navbar-dark w-100">
+        <div class="container-fluid">
+            <a class="navbar-brand text-uppercase  fw-bolder mx-4 py-3" href="/">
+            <img src="./img/ang4.jpg" alt="" width="50" height="50">
+            </a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-md-auto">
+                    <li class="nav-item pe-4">
+                        <a class="nav-link active" href="localhost:8888/MonguideAnglais">Accueil</a>
+                    </li>
+                    <li class="nav-item dropdown pe-4">
+                        <a class="nav-link active" href="onglet1.php">Abonnement</a>
+                    </li>
+                    <li class="nav-item pe-4">
+                        <a class="nav-link active" href="https://www.ef.fr/blog/language/verite-football-anglais/">Tarif</a>
+                    </li>
+                    <li class="nav-item pe-4">
+                        <a class="nav-link active" href="https://www.visitlondon.com/fr">tester un code</a>
+                    </li>
+                </ul>
+            </div>
+            
+            </div>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item pe-4">
+                        <a class="nav-link active" href=#>Se connecter</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    
+
+                
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <!-- <div class="btn-group">
+    <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+    Réservation 
+    </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="onglet4.php">Réservation en ligne</a></li>
+            <li><a class="dropdown-item" href="onglet3.php">Stock en direct</a></li>
+        </ul> 
+    </div> -->
+    
+</body>
+
+<!--<footer class="bg-secondary">
+    <div class="containeur">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-white text capitalize font-weight-light p-3">open anchor company<h1>
+                <h3 class="text-light font-weight font italic mb-3">Ouvert tout les jours</h3>
+            </div>
+        </div>
+    </div>     
+</footer>-->
+<!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</html>
