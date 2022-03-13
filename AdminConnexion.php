72f37ff209220cd2f5dcd5fea8312298141aebf8
Aikioo <timpoknews@gmail.com>
2022-03-13 12:02:44 +0100

    Accès Login/Mdp

--- AdminConnexion.php
+++ AdminConnexion.php
@@ -5,27 +5,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
     <title>Document</title>
 </head>
     <body>
         <div id="container">
             <!-- zone de connexion -->
             
-            <form action="target1.php" method="POST">
+            <?php include_once "admin/inc/script_login.php"?>
+            <?php if(isset($erreur)){echo $erreur;}?>
+
+            <!-- <form action="" method="POST">
                 <h1>Connexion</h1>
                 
                 <label for="mdp">Mot de passe</label> : <br /><input type="text" placeholder="Entrer votre mot de passe" name="mdp" /><br /><br />
 
                 <input type="submit" id='submit' value='LOGIN' >
-                <?php
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2)
-                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-                }
-                ?>
-            </form>
+
+            </form> -->
+
+            <form action="" method="post">
+                <input class="" type="text" name="idAdmin" placeholder="ID">
+                <input class="" type="password" name="mdp" placeholder="Mot de passe">
+                <button type="submit" name="submit"> Se connecter </button>
+
+
+
         </div>
     </body>
 </html>
