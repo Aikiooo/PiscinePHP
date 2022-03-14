73e675526b7aa1f57e59c3996a781285c3d42db6
guillaumetroyaux <98318805+guillaumetroyaux@users.noreply.github.com>
2022-03-14 14:43:03 +0100

    maj

--- AccLambda.php
+++ AccLambda.php
@@ -8,24 +8,13 @@
     <link rel="shortcut icon" href="./imgP/logoville.jpg">
     
     <title>Accueil</title>
-    <?php
-    session_start();
-    if(session_id('admin')) {
-        require_once('navbarAdmin.php');
-    }
-    else {
-    require_once('navbarLambda.php');
-    }
-
-        
-        
-    ?>
 <head>
 </head>
 
 <body>
     <?php
         require_once('navbarLambda.php')
+        
     ?>
     <p>
     <br />
@@ -58,39 +47,49 @@
     </div>
 
 </body>
-<footer>
-</footer>
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-    <!-- <div class="btn-group">
-    <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-    Réservation 
-    </button>
-        <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="onglet4.php">Réservation en ligne</a></li>
-            <li><a class="dropdown-item" href="onglet3.php">Stock en direct</a></li>
-        </ul> 
-    </div> -->
-    
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Untitled</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
 
-
-<!--<footer class="bg-secondary">
-    <div class="containeur">
-        <div class="row">
-            <div class="col">
-                <h1 class="text-white text capitalize font-weight-light p-3">open anchor company<h1>
-                <h3 class="text-light font-weight font italic mb-3">Ouvert tout les jours</h3>
+<body>
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Accessibilité</h3>
+                        <ul>
+                            <li><a href="#">Nous contacter</a></li>
+                            <li><a href="#">Plan du site</a></li>
+                            <li><a href="#">Rennes métropole</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>A propos</h3>
+                        <ul>
+                            <li><a href="#">Données personnelles</a></li>
+                            <li><a href="#">Mentions légales</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Rennes métropole</h3>
+                        <p>Hôtel de Rennes métropole 4, avenue Henri Fréville CS 93111 35031 Rennes Cedex
+                        </p>
+                    </div>
+                    <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                </div>
+                <p class="copyright">Rennes métropole © 2022</p>
             </div>
-        </div>
-    </div>     
-</footer>-->
-<!-- Optional JavaScript; choose one of the two! -->
+        </footer>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+</body>
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
 </html>
